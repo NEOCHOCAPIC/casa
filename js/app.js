27dad7342ae8dd05cc45e02c6a4171f1f3776eb7
NEOCHOCAPIC <129566811+NEOCHOCAPIC@users.noreply.github.com>
2024-02-29 19:08:03 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
+
+    darkMode();
 });
+
+
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode1');
+    }else {
+        document.body.classList.remove('dark-mode1');
+    }
+
+    const botonDarkMode = document.querySelector('.dark-mode');
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode1');
+    });
+
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode1');
+        }else {
+            document.body.classList.remove('dark-mode1');
+        }
+    });
+}
+
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.menu-telefono');
